fd4a63844c6b0e5ed0985bc1a6d8d114183e3f21
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-10-08 12:06:21 +0300

    add

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,6 +28,7 @@ TransactionHistory.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
       currency: PropTypes.string.isRequired,
     })
   ),
